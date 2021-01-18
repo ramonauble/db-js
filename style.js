f40089fba18991ae0b842ded6fe5913e284a51d3
ramona <ramonaauble@gmail.com>
2021-01-18 12:57:15 -0500

    +page select indicators/colors, subtle ui sizing

---
+++ style.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $(".uiButton, .pageButton").hover(function() {
+    $(this).css("opacity", "100%");
+  }, function() {
+    $(this).css("opacity", "81%");
+  }).click(function() {
+
+  });
+});
+
+
+//purple, red, black, green, blue, grey
+//AE77D2 - purple
+//E93F3F - red
+//000000 - black
+//7CDE89 - green
+//7CA7DE - blue
+//999999 - grey
