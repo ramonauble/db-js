a0ebe9a52c0a3f01f35b8ba64b331f76c6f35bf4
ramona <ramonaauble@gmail.com>
2021-01-18 14:36:27 -0500

    +reactive styling for active synth & ui pages

--- style.js
+++ style.js
@@ -1,10 +1,81 @@
+'use strict';
+
 $(document).ready(function() {
+  var activePage = "oscButton";
+  var activeUI = "infoButton";
+
+  var $pageDict = {
+    oscButton: $("#oscButton"),
+    ratButton: $("#ratButton"),
+    ofxButton: $("#ofxButton"),
+    panButton: $("#panButton"),
+    ampButton: $("#ampButton"),
+    lfoButton: $("#lfoButton")
+  };
+  var $uiDict = {
+    infoButton: $("#infoButton"),
+    waveButton: $("#waveButton")
+  };
+
+  var $pageSelectDict = {
+    oscButton: $("#oscSelect"),
+    ratButton: $("#ratSelect"),
+    ofxButton: $("#ofxSelect"),
+    panButton: $("#panSelect"),
+    ampButton: $("#ampSelect"),
+    lfoButton: $("#lfoSelect")
+  };
+  var $uiSelectDict = {
+    infoButton: $("#infoSelect"),
+    waveButton: $("#waveSelect")
+  };
+
+  var $activePage = $pageDict[activePage];
+  var $activePageSelect = $pageSelectDict[activePage];
+
+  var $activeUI = $uiDict[activeUI];
+  var $activeUISelect = $uiSelectDict[activeUI];
+
+  $activePage.css("opacity", "100%");
+  $activePageSelect.css("opacity", "100%");
+  $activeUI.css("opacity", "100%");
+  $activeUISelect.css("opacity", "100%");
+
+
   $(".uiButton, .pageButton").hover(function() {
     $(this).css("opacity", "100%");
   }, function() {
-    $(this).css("opacity", "81%");
+    let $this = $(this);
+    if ($this.hasClass("pageButton")) {
+      if ($this.attr("id") != activePage) {
+        $this.css("opacity", "81%");
+      }
+    } else {
+      if ($this.attr("id") != activeUI) {
+        $this.css("opacity", "81%");
+      }
+    }
   }).click(function() {
-
+    let $this = $(this);
+    if ($this.hasClass("pageButton")) {
+      if ($this.attr("id") != activePage) {
+        $activePage.css("opacity", "81%");
+        $this.css("opacity", "100%");
+        $pageSelectDict[activePage].css("opacity", "25%");
+        activePage = $this.attr("id");
+        $activePage = $pageDict[activePage];
+        $pageSelectDict[activePage].css("opacity", "100%");
+      }
+    } else {
+      if ($this.attr("id") != activeUI) {
+        $activeUI.css("opacity", "81%");
+        $this.css("opacity", "100%");
+        $uiSelectDict[activeUI].css("opacity", "25%");
+        activeUI = $this.attr("id");
+        $activeUI = $uiDict[activeUI];
+        $uiSelectDict[activeUI].css("opacity", "100%");
+      }
+    }
   });
 });
 
