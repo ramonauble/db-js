7e54a2cad2e6d84f805bbdb47f2cbe48461da28c
ramona <ramonaauble@gmail.com>
2021-04-12 13:04:34 -0400

    +custom ADSR envelope node

--- worklets.js
+++ worklets.js
@@ -172,6 +172,11 @@ class additiveOsc extends AudioWorkletProcessor {
     let iRatio = parameters.ratio[0];
     let freqHasChanged = !(frequency.length === 1);
     let ratioHasChanged = !(ratio.length === 1);
+    let newFreq = 0;
+    let normPhase = 0;
+    let newPhase = 0;
+    let newSamp = 0;
+    let n = 0;
     const pi = Math.PI;
 
     for (let i = 0; i < outputs[0][0].length; i++) {
@@ -181,18 +186,190 @@ class additiveOsc extends AudioWorkletProcessor {
       if (ratioHasChanged) {
         iRatio = ratio[i];
       }
-      let newFreq = iRatio * iFreq; //calc new frequency
+      newFreq = iRatio * iFreq; //calc new frequency
       //calculate phase increment
         //newFreq/sampleRate gives # of cycles per sample
         //multiplying by max gives accumulator increment
       this.incr = this.max * (newFreq/(sampleRate));
       this.phase[0] += this.incr; //increment accumulator
-      let normPhase = this.phase[0]/(this.max);
-      let newPhase = 2*pi*normPhase; //normalized to radians
-      let newSamp = Math.sin(newPhase);
-      for (let n = 0; n < outputs.length; n++) {
+      normPhase = this.phase[0]/(this.max);
+      newPhase = 2*pi*normPhase; //normalized to radians
+      newSamp = Math.sin(newPhase);
+      for (n = 0; n < outputs.length; n++) {
         //output n, channel 0, sample i
         outputs[n][0][i] = newSamp;
+      }
+    }
+    return true;
+  }
+}
+
+class envelopeNode extends AudioWorkletProcessor {
+  static get parameterDescriptors() {
+    return [
+      {
+        name: "attack",
+        defaultValue: .1,
+        minValue: 0,
+        maxValue: 1,
+        automationRate: "a-rate"
+      },
+      {
+        name: "decay",
+        defaultValue: .5,
+        minValue: 0,
+        maxValue: 1,
+        automationRate: "a-rate"
+      },
+      {
+        name: "sustain",
+        defaultValue: 0,
+        minValue: 0,
+        maxValue: 1,
+        automationRate: "a-rate"
+      },
+      {
+        name: "release",
+        defaultValue: .5,
+        minValue: 0,
+        maxValue: 1,
+        automationRate: "a-rate"
+      },
+      {
+        name: "state",
+        defaultValue: 0,
+        minValue: 0,
+        maxValue: 1,
+        automationRate: "a-rate"
+      }
+    ];
+  }
+
+  constructor() {
+    super();
+
+    this.acc = new Uint32Array(1); //envelope accumulator state
+    this.prevAcc = 0; //previous accumulator state (for stage change)
+    this.accBuff = 0; //accumulator buffer (for release stage)
+    this.max = (Math.pow(2, 32) - 1); //accumulator max
+    this.inc = 0; //sample increment for current stage
+    this.prevState = 0; //previous envelope state
+    this.stage = 0; //init stage to not running
+      //1 - attack, 2 - decay, 3 - sustain, 4 - release
+  }
+
+  process(inputs, outputs, parameters) {
+    let output = outputs[0][0]; //output 0, channel 0
+
+    let currentState = parameters.state;
+    let currentAttack = parameters.attack;
+    let currentDecay = parameters.decay;
+    let currentSustain = parameters.sustain;
+    let currentRelease = parameters.release;
+
+    let stateHasChanged = !(currentState.length === 1);
+    let attackHasChanged = !(currentAttack.length === 1);
+    let decayHasChanged = !(currentDecay.length === 1);
+    let sustainHasChanged = !(currentSustain.length === 1);
+    let releaseHasChanged = !(currentRelease.length === 1);
+
+    let sampleState;
+    let sampleAttack;
+    let sampleDecay;
+    let sampleSustain;
+    let sampleRelease;
+
+    for (let i = 0; i < output.length; i++) {
+      if (stateHasChanged) {
+        sampleState = currentState[i];
+      } else {
+        sampleState = currentState[0];
+      }
+      if (attackHasChanged) {
+        sampleAttack = currentAttack[i];
+      } else {
+        sampleAttack = currentAttack[0];
+      }
+      if (decayHasChanged) {
+        sampleDecay = currentDecay[i];
+      } else {
+        sampleDecay = currentDecay[0];
+      }
+      if (sustainHasChanged) {
+        sampleSustain = currentSustain[i];
+      } else {
+        sampleSustain = currentSustain[0];
+      }
+      if (releaseHasChanged) {
+        sampleRelease = currentRelease[i];
+      } else {
+        sampleRelease = currentRelease[0];
+      }
+
+      if (this.prevState == 0 && sampleState == 1) { //note on
+        this.acc = 0; //init accumulator
+        this.prevAcc = 0;
+        this.accBuff = 0;
+        this.stage = 1; //initiate attack stage
+        this.prevState = sampleState; //save state for current sample
+      } else if (this.prevState == 1 && sampleState == 0) { //note off
+        this.stage = 4; //initiate release stage
+        this.prevState = sampleState; //save state for current sample
+      }
+
+      if (this.stage == 1) { //attack stage
+        if (sampleAttack == 0) {
+          this.inc = this.max;  //instant attack
+          this.stage = 2;       //trigger decay on next sample
+        } else {
+          this.inc = this.max/(sampleAttack * sampleRate); //calculate increment
+        }
+        this.prevAcc = this.acc;  //save current accumulator state
+        this.acc += this.inc;          //increment accumulator
+        if (this.acc < this.max) {  //attack phase still in progress
+          output[i] = this.acc/this.max //output between 0-1
+        } else {              //accumulator overflow - trigger decay stage
+          this.acc = this.max;//set accumulator to maximum
+          output[i] = 1.0;    //end of sttack - output at max
+          this.stage = 2;     //initiate decay stage - on next sample
+        }
+        this.accBuff = this.acc; //save accumulator state to buffer
+      } else if (this.stage == 2) { //decay stage
+        if (sampleDecay == 0) {
+          this.inc = (this.max - this.max*sampleSustain); //dec directly to sustain
+          this.stage = 3;
+        } else {
+          this.inc = (this.max - this.max*sampleSustain)/(sampleDecay * sampleRate);
+        }
+        this.prevAcc = this.acc;
+        this.acc -= this.inc; //decrement to sustain
+        if (this.acc > (this.max*sampleSustain)) {
+          output[i] = this.acc/this.max //output between 0-1
+        } else {
+          this.acc = this.max * sampleSustain;
+          output[i] = 1.0 * sampleSustain;
+          this.stage = 3;
+        }
+        this.accBuff = this.acc; //save accumulator state to buffer
+      } else if (this.stage == 3) {
+        this.acc = this.max * sampleSustain;
+        this.accBuff = this.acc;
+        output[i] = 1.0 * sampleSustain; //hold until release
+      } else if (this.stage == 4) {
+        if (sampleRelease == 0) { //instant release
+          this.inc = this.accBuff;
+          this.stage = 0; //end of envelope
+        } else {
+          this.inc = this.accBuff/(sampleRelease * sampleRate)
+        }
+        this.prevAcc = this.acc;
+        this.acc -= this.inc;
+        if (this.acc > 0) {
+          output[i] = this.acc/this.max;
+        } else {
+          output[i] = 0;
+          this.stage = 0; //end envelope
+        }
       }
     }
     return true;
@@ -202,3 +379,4 @@ class additiveOsc extends AudioWorkletProcessor {
 registerProcessor("gainProcessor", gainProcessor);
 registerProcessor("panProcessor", panProcessor);
 registerProcessor("additiveOsc", additiveOsc);
+registerProcessor("envelopeNode", envelopeNode);
